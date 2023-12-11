ac48c74c8bbd7bcf2308654358cdd848a8012f16
Ichorix <multiply2by2@gmail.com>
2023-12-11 11:36:43 -0500

    Remade Horn physics with IK

--- Assets/Scripts/Player/Updated Scripts/NPlayerProceduralAnimations.cs
+++ Assets/Scripts/Player/Updated Scripts/NPlayerProceduralAnimations.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPlayerLegAnimations : MonoBehaviour
+public class NPlayerProceduralAnimations : MonoBehaviour
 {
     public Transform intendedTarget;
     public float animationSpeed;
     private float distanceApart;
+    public float gravityDifference;
 
     void Update()
     {
         distanceApart = Vector3.Distance(transform.position, intendedTarget.position);
         float step = animationSpeed * distanceApart * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, intendedTarget.position, step);
+        transform.position = Vector3.MoveTowards(transform.position, intendedTarget.position + new Vector3(0, gravityDifference, 0), step);
     }
 }
