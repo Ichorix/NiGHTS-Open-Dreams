58d5c51a56d869ba49389f124e3a811757fa91bb
Ichorix <multiply2by2@gmail.com>
2023-12-20 20:44:45 -0500

    Options PlayerPrefs

--- NiGHTS Open Dreams/Assets/UI/Menu/Scripts/OptionsManager.cs
+++ NiGHTS Open Dreams/Assets/UI/Menu/Scripts/OptionsManager.cs
@@ -8,25 +8,31 @@ using Cinemachine;
 
 public class OptionsManager : MonoBehaviour
 {
-    public GameObject keybindingsMenu;
-    public GameObject accessibilitiesMenu;
-    public GameObject displayMenu;
-    public GameObject audioMenu;
-    public Button keybindsB;
-    public Button displayB;
-    public Button audioB;
-    public Button accessB;
-
-    public AudioMixer audioMixer;
-    public Text masterAudioText;
-    public TMPro.TMP_Dropdown resolutionDropdown;
+    [SerializeField] private GameObject keybindingsMenu;
+    [SerializeField] private GameObject accessibilitiesMenu;
+    [SerializeField] private GameObject displayMenu;
+    [SerializeField] private GameObject audioMenu;
+    [SerializeField] private Button keybindsB;
+    [SerializeField] private Button displayB;
+    [SerializeField] private Button audioB;
+    [SerializeField] private Button accessB;
+    [Space]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider soundsSlider;
+    [SerializeField] private Slider ambienceSlider;
+    [SerializeField] private TMPro.TMP_Dropdown resolutionDropdown;
     Resolution[] resolutions;
 
     public CinemachineFreeLook cameraSettings;
-    public Button playerSpace;
-    public Button worldSpace;
-
     void Start()
+    {
+        GetResolutionOptions();
+        LoadAudioValues();
+        
+    }
+    private void GetResolutionOptions()
     {
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
@@ -117,58 +123,56 @@ public class OptionsManager : MonoBehaviour
         Screen.SetResolution(setResolution.width, setResolution.height, Screen.fullScreen);
     }
 
-    //////////Audio////////////
+    //////////Audio//////////
+    private void LoadAudioValues()
+    {
+        if(PlayerPrefs.HasKey("masterVolume"))
+            masterSlider.value = PlayerPrefs.GetFloat("masterVolume");
+        SetMasterVolume(masterSlider.value);
+
+        if(PlayerPrefs.HasKey("musicVolume"))
+            musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SetMusicVolume(musicSlider.value);
+
+        if(PlayerPrefs.HasKey("sfxVolume"))
+            soundsSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetFXVolume(soundsSlider.value);
+
+        if(PlayerPrefs.HasKey("ambienceVolume"))
+            ambienceSlider.value = PlayerPrefs.GetFloat("ambienceVolume");
+        SetAmbienceVolume(ambienceSlider.value);
+    }
     public void SetMasterVolume(float volumeValue)
     {
-        audioMixer.SetFloat("masterVolume", volumeValue);
+        audioMixer.SetFloat("masterVolume", Mathf.Log10(volumeValue) * 20);
+        PlayerPrefs.SetFloat("masterVolume", volumeValue);
     }
     public void SetMusicVolume(float volumeValue)
     {
-        audioMixer.SetFloat("musicVolume", volumeValue);
+        audioMixer.SetFloat("musicVolume", Mathf.Log10(volumeValue) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volumeValue);
     }
     public void SetFXVolume(float volumeValue)
     {
-        audioMixer.SetFloat("sfxVolume", volumeValue);
+        audioMixer.SetFloat("sfxVolume", Mathf.Log10(volumeValue) * 20);
+        PlayerPrefs.SetFloat("sfxVolume", volumeValue);
     }
     public void SetAmbienceVolume(float volumeValue)
     {
-        audioMixer.SetFloat("ambienceVolume", volumeValue);
+        audioMixer.SetFloat("ambienceVolume", Mathf.Log10(volumeValue) * 20);
+        PlayerPrefs.SetFloat("ambienceVolume", volumeValue);
     }
 
-    //////////Accessibilities//
-    public void CamSetWorld()
-    {
-        if(cameraSettings != null)
-        {
-            cameraSettings.m_BindingMode = CinemachineTransposer.BindingMode.SimpleFollowWithWorldUp;
-            playerSpace.interactable = true;
-            worldSpace.interactable = false;
-        }
-    }
-    public void CamSetPlayer()
-    {
-        if(cameraSettings != null)
-        {
-            cameraSettings.m_BindingMode = CinemachineTransposer.BindingMode.LockToTarget;
-            playerSpace.interactable = false;
-            worldSpace.interactable = true;
-        }
-    }
+    //////////Accessibilities//////////
     public void SetYInverted(bool YInvert)
     {
         if(cameraSettings != null)
-        {
             cameraSettings.m_YAxis = new AxisState(0, 1, false, true, 2f, 0.2f, 0.1f, "Mouse Y", YInvert);
-            Debug.Log("Y axis inversion is " + YInvert);
-        }
     }
     public void SetXInverted(bool XInvert)
     {
         if(cameraSettings != null)
-        {
             cameraSettings.m_XAxis = new AxisState(-180, 180, true, false, 300f, 0.1f, 0.1f, "Mouse X", XInvert);
-            Debug.Log("X axis inversion is " + XInvert);
-        }
     }
 
 }
