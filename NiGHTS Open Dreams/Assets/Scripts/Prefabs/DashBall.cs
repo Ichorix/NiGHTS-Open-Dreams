fa8281f934e3e671428f273a715d9a2a21aa888b
Ichorix <multiply2by2@gmail.com>
2023-12-22 18:41:49 -0500

    Path 2 Update

--- NiGHTS Open Dreams/Assets/Scripts/Prefabs/DashBall.cs
+++ NiGHTS Open Dreams/Assets/Scripts/Prefabs/DashBall.cs
@@ -11,22 +11,21 @@ public class DashBall : MonoBehaviour
     public float upSpeed;
     public ForceMode upForceMode;
 
-    private NPlayerLevelFollow levelFollow;
+    [SerializeField] private NPlayerLevelFollow levelFollow;
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger Enter");
-        levelFollow = other.gameObject.GetComponent<NPlayerLevelFollow>();
+        //levelFollow = other.gameObject.GetComponent<NPlayerLevelFollow>();
 
         if(levelFollow != null)
             StartCoroutine(Launch());
     }
 
-    // If youre getting a MoveNext() error in this Coroutine, I have absolutely no idea how to fix it
     IEnumerator Launch()
     {
-        Debug.Log("Launch");
-        levelFollow.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * upSpeed, upForceMode);
+        if(upSpeed != 0)
+            levelFollow.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * upSpeed, upForceMode);
+
         timeLeft = launchTime;
         while (timeLeft > 0)
         {
