54717e5ed93e60fa0f5f3b5cdf7b10e6973a75e6
Ichorix <multiply2by2@gmail.com>
2024-01-06 13:57:20 -0500

    Fixed up scene loading for build

--- NiGHTS Open Dreams/Assets/NiGHTS-Open-Dreams-Resources/Scripts/Menu/SAGESPLASH.cs
+++ NiGHTS Open Dreams/Assets/NiGHTS-Open-Dreams-Resources/Scripts/Menu/SAGESPLASH.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SAGESPLASH : MonoBehaviour
 {
     [SerializeField] private Slider loadingBar;
+    [SerializeField] private TextMeshProUGUI percentageText;
     [SerializeField] private VideoPlayer videoPlayer;
 
     void Start()
@@ -24,11 +26,12 @@ public class SAGESPLASH : MonoBehaviour
 
     private IEnumerator LoadingProgress()
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync("TerrainMaker");
+        AsyncOperation operation = SceneManager.LoadSceneAsync("OpenDreams");
         while(!operation.isDone)
         {
-            float progress = Mathf.Clamp01(operation.progress * 16);
-            loadingBar.value = progress;
+            float progress = operation.progress; // 0 - 1
+            percentageText.text = progress * 100 + "%";
+            loadingBar.value = Mathf.Clamp(progress * 16, 1, 16);
             yield return null;
         }
     }
