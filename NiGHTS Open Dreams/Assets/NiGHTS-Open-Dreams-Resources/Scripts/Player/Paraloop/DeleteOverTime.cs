7fbbf379432d7a292e2072be7e59eb395cf7974d
Ichorix <multiply2by2@gmail.com>
2024-01-08 18:37:43 -0500

    Fixed deleteOverTime bug

--- NiGHTS Open Dreams/Assets/NiGHTS-Open-Dreams-Resources/Scripts/Player/Paraloop/DeleteOverTime.cs
+++ NiGHTS Open Dreams/Assets/NiGHTS-Open-Dreams-Resources/Scripts/Player/Paraloop/DeleteOverTime.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class DeleteOverTime : MonoBehaviour
 {
     [SerializeField] private float timeToDelete = 1.5f;
+    private bool markedForDelete = false;
     IEnumerator Start()
     {
+        markedForDelete = true;
         yield return new WaitForSeconds(timeToDelete);
         Destroy(gameObject);
+    }
+    void OnEnable()
+    {
+        // Using this method because if the gameObject was disabled when the WaitForSeconds() is up, then it would not actually destroy the object
+        if(markedForDelete)
+            Destroy(gameObject);
     }
 }
